75883610df58fbebaad293c2fe47913b50bbac32
austinkwok1997 <austin.kwok@protonmail.com>
2019-06-20 10:51:54 -0700

    turned cards into components

--- imports/ui/components/Body.jsx
+++ imports/ui/components/Body.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import RestaurantCard from './RestaurantCard'
 
 export default class Body extends Component {
   render() {
@@ -6,97 +7,36 @@ export default class Body extends Component {
       <div className="album py-5 bg-light">
         <div className="container">
           <div className="row">
-
-            <div className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <img className="bd-placeholder-img card-img-top" src="https://vignette.wikia.nocookie.net/itsalwayssunny/images/e/e1/1x1_Paddys.png/revision/latest/scale-to-width-down/640?cb=20110709045709" />
-                <div className="card-body">
-                  <p className="card-text">Paddy's Irish Pub</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                    <small className="text-muted">Philadelphia, PA</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <img className="bd-placeholder-img card-img-top" src="https://vignette.wikia.nocookie.net/itsalwayssunny/images/e/e1/1x1_Paddys.png/revision/latest/scale-to-width-down/640?cb=20110709045709" />
-                <div className="card-body">
-                  <p className="card-text">Paddy's Irish Pub</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                    <small className="text-muted">Philadelphia, NY</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <img className="bd-placeholder-img card-img-top" src="https://vignette.wikia.nocookie.net/itsalwayssunny/images/e/e1/1x1_Paddys.png/revision/latest/scale-to-width-down/640?cb=20110709045709" />
-                <div className="card-body">
-                  <p className="card-text">Paddy's Irish Pub</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                    <small className="text-muted">Philadelphia, NY</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <img className="bd-placeholder-img card-img-top" src="http://www.manchestersfinest.com/wp-content/uploads/2019/06/central-perk.png" />
-                <div className="card-body">
-                  <p className="card-text">Central Perk Coffee House</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                    <small className="text-muted">New York, NY</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <img className="bd-placeholder-img card-img-top" src="http://www.manchestersfinest.com/wp-content/uploads/2019/06/central-perk.png" />
-                <div className="card-body">
-                  <p className="card-text">Central Perk Coffee House</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                    <small className="text-muted">New York, NY</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <img className="bd-placeholder-img card-img-top" src="http://www.manchestersfinest.com/wp-content/uploads/2019/06/central-perk.png" />
-                <div className="card-body">
-                  <p className="card-text">Central Perk Coffee House</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                    </div>
-                    <small className="text-muted">New York, NY</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
+            <RestaurantCard
+              photo="https://vignette.wikia.nocookie.net/itsalwayssunny/images/e/e1/1x1_Paddys.png/revision/latest/scale-to-width-down/640?cb=20110709045709"
+              name="Paddy's Irish Pub"
+              location="Philadelphia, NY"
+            />
+            <RestaurantCard
+              photo="https://vignette.wikia.nocookie.net/itsalwayssunny/images/e/e1/1x1_Paddys.png/revision/latest/scale-to-width-down/640?cb=20110709045709"
+              name="Paddy's Irish Pub"
+              location="Philadelphia, NY"
+            />
+            <RestaurantCard
+              photo="https://vignette.wikia.nocookie.net/itsalwayssunny/images/e/e1/1x1_Paddys.png/revision/latest/scale-to-width-down/640?cb=20110709045709"
+              name="Paddy's Irish Pub"
+              location="Philadelphia, NY"
+            />
+            <RestaurantCard
+              photo="http://www.manchestersfinest.com/wp-content/uploads/2019/06/central-perk.png"
+              name="Central Perk Coffee House"
+              location="New York, NY"
+            />
+            <RestaurantCard
+              photo="http://www.manchestersfinest.com/wp-content/uploads/2019/06/central-perk.png"
+              name="Central Perk Coffee House"
+              location="New York, NY"
+            />
+            <RestaurantCard
+              photo="http://www.manchestersfinest.com/wp-content/uploads/2019/06/central-perk.png"
+              name="Central Perk Coffee House"
+              location="New York, NY"
+            />
           </div>
         </div>
       </div>
