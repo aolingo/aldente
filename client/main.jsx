6fae19185f37d8d60157b3276d31a8fdce9f3f5a
v4w9a <v4w9a@ugrad.cs.ubc.ca>
2019-06-23 20:08:15 -0700

    restructured and refactored project, added integration with social media logins and mongodb, pair programming with George

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../imports/ui/reducers';
+
 import App from '/imports/ui/App'
+import '../imports/startup/accounts-config';
 
 function main() {
   return (
--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import Links from '/imports/api/links';
+import Restaurants from '/imports/api/restaurants';
 
 function insertLink(title, url) {
   Links.insert({ title, url, createdAt: new Date() });
@@ -7,25 +7,26 @@ function insertLink(title, url) {
 
 Meteor.startup(() => {
   // If the Links collection is empty, add some data.
-  if (Links.find().count() === 0) {
-    insertLink(
-      'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
-    );
 
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
-
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
-
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
-  }
+  /*  if (Links.find().count() === 0) {
+      insertLink(
+        'Do the Tutorial',
+        'https://www.meteor.com/tutorials/react/creating-an-app'
+      );
+  
+      insertLink(
+        'Follow the Guide',
+        'http://guide.meteor.com'
+      );
+  
+      insertLink(
+        'Read the Docs',
+        'https://docs.meteor.com'
+      );
+  
+      insertLink(
+        'Discussions',
+        'https://forums.meteor.com'
+      );
+    } */
 });
