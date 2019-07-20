77baf07b14125b215577fac4e1c76502a8189564
austinkwok1997 <austin.kwok@protonmail.com>
2019-07-19 23:57:19 -0700

    removed some imports

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,10 +8,6 @@ import Owner from './components/Pages/Dashboard/Owner';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Contact from './components/Pages/Contact';
 import Restaurant from './components/Pages/Restaurants/Restaurant';
-import SignUp from './components/Auth/SignUp';
-import Login from './components/Auth/Login';
-import NoMatch from './components/Pages/NoMatch';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => (
   <React.Fragment>
@@ -23,8 +19,6 @@ const App = () => (
         <Route path='/dashboard/customer' component={Customer} />
         <Route path='/dashboard/owner' component={Owner} />
         <Route path='/contact' component={Contact} />
-        <Route path='/signup' component={SignUp} />
-        <Route path='/login' component={Login} />
         <Route path='/restaurant=:id' component={Restaurant} />
         <Route component={NoMatch} />
       </Switch>
