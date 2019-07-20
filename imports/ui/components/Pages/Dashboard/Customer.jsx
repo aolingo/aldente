12093ca8bbdea438f51f85f60a3e55bc326884f7
Ao Lin <gongzhilin@hotmail.com>
2019-07-19 23:09:19 -0700

    added customer and owner dashboard to be rendered on menu

---
+++ imports/ui/components/Pages/Dashboard/Customer.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Customer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+        <h1>Customer Dashboard</h1>
+
+      </div>
+    )
+  }
+}
---
+++ imports/ui/components/Pages/Dashboard/Owner.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Owner extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Owner Dashboard</h1>
+      </div>
+    )
+  }
+}
